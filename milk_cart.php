3f97be417c62499bc2881006c359ab7c0506c31b
LuuSean <cuong.luusean@gmail.com>
2017-12-22 00:44:06 +0700

    updated in 22/12/17 12:43am

--- milk_cart.php
+++ milk_cart.php
@@ -1,16 +1,35 @@
-<table>
+<table class="table">
 	<thead>
 		<tr>
-			<th>Employee number</th>
-			<th>Employee firstname</th>
-			<th>Employee last name</th>
-			<th>Employee email</th>
-			<th>Employee job title</th>
+			<th>STT</th>
+			<th>MaHH</th>
+			<th>TenHH</th>
+			<th>GiaBan</th>
+			<th>DonVi</th>
+			<th>HinhAnh</th>
 		</tr>
 	</thead>
 	<tbody>
+		<?php 
+			$conn = mysqli_connect("localhost", "LuuSean", "9704061342284595", "milk_for_baby");
+			//check connection
+			if (mysqli_connect_error()) {
+				echo "Failed to connect to MySQL: ".mysqli_connect_error();
+			}
+
+			$result = mysqli_query($conn, "SELECT * FROM hanghoa");
+
+			while ($row = mysqli_fetch_assoc($result)):
+		?>
+
 		<tr>
-			<td>data</td>
+			<td><?php echo $row["MaHH"]; ?></td>
+			<td><?php echo $row["TenHH"]; ?></td>
+			<td><?php echo $row["GiaBan"]; ?></td>
+			<td><?php echo $row["DVT"]; ?></td>
+			<td><?php echo $row["HinhAnh"]; ?></td>
 		</tr>
+
 	</tbody>
 </table>
+<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
