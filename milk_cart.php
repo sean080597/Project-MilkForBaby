e5fa2eba7b93d5db1a6cc05ea06684ddf1d73c95
LuuSean <cuong.luusean@gmail.com>
2017-12-21 12:55:22 +0700

    added milk_cart.php

---
+++ milk_cart.php
@@ -0,0 +1,16 @@
+<table>
+	<thead>
+		<tr>
+			<th>Employee number</th>
+			<th>Employee firstname</th>
+			<th>Employee last name</th>
+			<th>Employee email</th>
+			<th>Employee job title</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>data</td>
+		</tr>
+	</tbody>
+</table>
