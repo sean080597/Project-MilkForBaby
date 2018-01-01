b1aaa1726c8ab8af403cd099c3ccc98010cb3556
LuuSean <cuong.luusean@gmail.com>
2018-01-02 01:14:40 +0700

    Updated

--- milk_add_new.php
+++ milk_add_new.php
@@ -328,7 +328,11 @@
                             </label>
                             <div class="col-sm">
 <<<<<<< HEAD
+<<<<<<< HEAD
                                  <select name="cbTypeItem" size="1" id="type_item">
+=======
+                                <select name="cbTypeItem" size=1 onChange="" id="type_item">
+>>>>>>> dc4067994811fc444ca27f1b79d4475906b38356
 =======
                                 <select name="cbTypeItem" size=1 onChange="" id="type_item">
 >>>>>>> dc4067994811fc444ca27f1b79d4475906b38356
